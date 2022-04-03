1f67287744057408a249c40f5c95910c8d6c1f4c
nickofolas <nickofolas@gmail.com>
2022-04-03 14:14:08 -0500

    Move again lol

--- overlay.user.js
+++ overlay.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/nickofolas/blursed_place/main/overlay.png?foo=barbazab";  // totally not messing with cache
+            i.src = "https://raw.githubusercontent.com/nickofolas/blursed_place/main/overlay.png?abc=z";  // totally not messing with cache
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
